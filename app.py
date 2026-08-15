fe01d4dcc243f2ee16984e686b43051456810b04
Madhuri Yepuri <madhuriyepuri098@gmail.com>
2026-08-15 15:15:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return send_from_directory(".", "index.html")
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    return jsonify({
+        "result": "FIRE DETECTED!",
+        "fire_area": "2.12%"
+    })
+@app.route("/script.js")
+def script():
+    return send_from_directory(".", "script.js")
+
+@app.route("/style.css")
+def style():
+    return send_from_directory(".", "style.css")
+if __name__ == "__main__":
+    app.run(debug=True)
